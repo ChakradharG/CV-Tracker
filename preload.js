a59e473f4d3178e101b3c45f6d9650b8ebbc6585
DG <gchakradhar72@gmail.com>
2021-06-27 22:17:42 +0530

    created preload to run nodejs code in frontend

---
+++ preload.js
@@ -0,0 +1,17 @@
+const { contextBridge, ipcRenderer } = require('electron');
+
+
+contextBridge.exposeInMainWorld('api', {
+	invoke: (channel, payLoad) => {
+		let validChannels = [];
+		if (validChannels.includes(channel)) {
+			return ipcRenderer.invoke(channel, payLoad);
+		}
+	},
+	send: (channel, payLoad) => {
+		let validChannels = [];
+		if (validChannels.includes(channel)) {
+			ipcRenderer.send(channel, payLoad);
+		}
+	}
+});
