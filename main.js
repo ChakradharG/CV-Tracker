c19e4ebd4a986f2752c38111418ded3931cf9054
DG <gchakradhar72@gmail.com>
2021-07-05 17:56:59 +0530

    Basics

--- main.js
+++ main.js
@@ -1,1 +1,31 @@
-console.log('Hello world');
+const { app, BrowserWindow, ipcMain } = require('electron');
+
+let win;
+require('electron-reload')(__dirname, {
+	electron: require(`${__dirname}/node_modules/electron`)
+});	// Remove
+
+function createWindow() {
+	win = new BrowserWindow({
+		webPreferences: {
+			preload: `${__dirname}/preload.js`
+		}
+	});
+
+	win.loadFile(`${__dirname}/public/index.html`);
+	win.webContents.openDevTools();	// Remove
+}
+
+app.whenReady().then(createWindow);
+
+app.on('activate', () => {
+	if (BrowserWindow.getAllWindows().length === 0) {
+		createWindow();
+	}
+});
+
+app.on('window-all-closed', () => {
+	if (process.platform !== 'darwin') {
+		app.quit();
+	}
+});
