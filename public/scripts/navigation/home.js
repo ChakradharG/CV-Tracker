4d5735111adb284e9039e3f784406f00b0f324e2
DG <gchakradhar72@gmail.com>
2021-08-15 00:21:54 +0530

    home tab done

--- public/scripts/navigation/home.js
+++ public/scripts/navigation/home.js
@@ -9,9 +9,10 @@ function parseDurationString(duration) {
 		return (new Date(str)).toDateString();
 	});
 
-	duration = `${from.slice(8, 10)} ${from.slice(4, 7)} ${from.slice(11, 15)}`;
+	duration = (from === 'Invalid Date') ? from : `${from.slice(8, 10)} ${from.slice(4, 7)} ${from.slice(11, 15)}`;
 	if (to) {
-		duration += ` - ${to.slice(8, 10)} ${to.slice(4, 7)} ${to.slice(11, 15)}`;
+		duration += ' - ';
+		duration += (to === 'Invalid Date') ? to : `${to.slice(8, 10)} ${to.slice(4, 7)} ${to.slice(11, 15)}`;
 	}
 
 	return duration;
@@ -59,6 +60,109 @@ function createModal(temporary) {
 	return modalContainer;
 }
 
+function createButton(className, innerText, listener){
+	let btn = document.createElement('button');
+	btn.className = className;
+	btn.innerText = innerText;
+	if (listener) {
+		btn.addEventListener(listener.ev, listener.callback);
+	}
+
+	return btn;
+}
+
+function update(entity) {
+	api.send('putData', JSON.stringify(entity));	// Reflect changes in the backend
+	api.invoke('getData').then((_data) => { data = _data });	// Reflect changes in the data object
+}
+
+function edit(entity) {
+	let container = createModal(true);
+	let div = container.firstChild;
+
+	let target = entity.target;
+
+	let input = (entity.type === 'row') ? document.createElement('textarea') : document.createElement('input');
+	input.value = entity.oldValue;
+	if (!(entity.tableID.match(/(mis)|(ski)/) && entity.column?.match(/Duration/))) {
+		input.placeholder = 'This field cannot be empty';
+	}
+	div.append(input);
+
+	if (entity.type === 'column') {
+		let _ = document.createElement('input');
+		_.type = 'checkbox';
+		_.checked = (entity.collapsibleColumn === entity.oldValue);
+		div.append(_);
+	}
+
+	if (entity.type === 'row' && (entity.column !== entity.collapsibleColumn)) {
+		div.append(createButton('modal-btn', 'Delete Row', {
+			ev: 'click',
+			callback: () => {
+				api.send('deleteData', JSON.stringify(entity));
+			}
+		}));
+	}
+
+	div.append(createButton('modal-btn', 'Cancel', {
+		ev: 'click',
+		callback: () => {
+			container.remove();
+		}
+	}));
+
+	div.append(createButton('modal-btn', 'Save', {
+		ev: 'click',
+		callback: () => {
+			let flag;
+			if (input.value === '' && !(entity.tableID.match(/(mis)|(ski)/) && entity.column?.match(/Duration/))) {
+				return;
+			}
+			if (entity.type === 'row') {
+				let value = input.value;
+				if (entity.column === 'Level') {
+					value = (value > 5)
+						? 5
+						: (value < 0)
+							? 0
+							: value;
+					value = '●'.repeat(value) + '○'.repeat((5 - value));
+				} else if (entity.column === 'Duration') {
+					value = parseDurationString(value);
+				}
+				target.innerHTML = value;
+			} else {
+				target.innerText = input.value;
+			}
+			entity.newValue = input.value;
+			if (entity.type === 'column') {
+				let _ = div.querySelector('input[type="checkbox"]');
+				if (entity.oldValue === entity.collapsibleColumn) {
+					if (!_.checked || (entity.oldValue !== entity.newValue)) {
+						entity.updateCol = true;
+						entity.collapsibleColumn = _.checked ? entity.newValue : null;
+						flag = true;
+					}
+				} else {
+					if (_.checked) {
+						entity.updateCol = true;
+						entity.collapsibleColumn = entity.newValue;
+						flag = true;
+					}
+				}
+			}
+			container.remove();
+			if ((entity.oldValue !== entity.newValue) || flag) {
+				update(entity);
+			}
+		}
+	}));
+	
+	document.querySelector('main').append(container);
+	container.style.display = 'flex';
+}
+
 function constructSection(element, heading, collapsibleColumn) {
 	let section = document.createElement('section');
 
@@ -68,14 +172,23 @@ function constructSection(element, heading, collapsibleColumn) {
 	sectionHeading.addEventListener('click', () => {
 		document.querySelector('.modal-container').style.display = 'flex';
 	});
+	sectionHeading.title = 'Right click to edit';
+	sectionHeading.addEventListener('contextmenu', () => {
+		edit({
+			type: 'table',
+			target: sectionHeading,
+			tableID: element[0],
+			oldValue: heading
+		});
+	});
 	section.append(sectionHeading);
 
 	let rows = element[1];
 	if (rows.length === 0) {
 		return section;
 	}
-
 	rows = assignRowSpan(rows, collapsibleColumn);
+
 	let table = document.createElement('table');
 
 	let hRow = document.createElement('tr');	// Head
@@ -84,7 +197,17 @@ function constructSection(element, heading, collapsibleColumn) {
 			return;
 		}
 		let _ = document.createElement('th');
-		_.innerHTML = key;
+		_.innerText = key;
+		_.title = 'Right click to edit';
+		_.addEventListener('contextmenu', () => {
+			edit({
+				type: 'column',
+				target: _,
+				tableID: element[0],
+				oldValue: key,
+				collapsibleColumn: collapsibleColumn
+			});
+		});
 		hRow.append(_);
 	});
 	table.append(hRow);
@@ -101,10 +224,23 @@ function constructSection(element, heading, collapsibleColumn) {
 					return;
 				}
 				_.rowSpan = row.rowSpan;
-			} else if (key === 'level') {	// Special column
+			}
+			_.title = 'Right click to edit';
+			_.addEventListener('contextmenu', () => {
+				edit({
+					type: 'row',
+					target: _,
+					tableID: element[0],
+					oldValue: row[key],
+					column: key,
+					id: row['id'],
+					collapsibleColumn: collapsibleColumn
+				});
+			});
+			if (key === 'Level') {	// Special column
 				value = '●'.repeat(value) + '○'.repeat((5 - value));
 
-			} else if (key === 'duration' && value) {	// Special column
+			} else if (key === 'Duration' && value) {	// Special column
 				value = parseDurationString(value);
 			}
 			_.innerHTML = value;
