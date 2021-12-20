ceb5c8a50a6058ede5ff819306444cea97a808de
DG <gchakradhar72@gmail.com>
2021-12-20 12:48:40 +0530

    Removed export tab

--- public/scripts/navigation/export.js
+++
@@ -1,6 +0,0 @@
-function renderExportTab() {
-	document.querySelector('.current').classList.remove('current');
-	document.querySelector('[title="Export"]').classList.add('current');
-
-	document.querySelector('main').innerHTML = 'Export';
-}
